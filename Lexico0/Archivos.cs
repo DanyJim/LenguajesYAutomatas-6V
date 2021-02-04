bf341a032e0b1a8c4d44fd2d992b68237371b1ad
Daniel Jiménez Galván <78568461+DanyJim@users.noreply.github.com>
2021-02-04 16:16:15 -0600

    Lexico0
    Levico0 - 6V

---
+++ Lexico0/Archivos.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lexico0
+{
+    class Archivos : IDisposable
+    {
+        StreamReader archivo;
+        StreamWriter bitacora;
+
+        public Archivos()
+        {
+            Console.WriteLine("Compilando el archivo Prueba.txt...");
+            if (File.Exists("C:\\Archivos\\Prueba.txt"))
+            {
+                archivo = new StreamReader("C:\\Archivos\\Prueba.txt");
+                bitacora = new StreamWriter("C:\\Archivos\\Prueba.log");
+                bitacora.AutoFlush = true;
+                bitacora.WriteLine("Archivo: Prueba.txt");
+                bitacora.WriteLine("Directorio: C:\\Archivos");
+            }
+            else
+            {
+                throw new Exception("El archivo Prueba.txt no existe");
+            }
+        }
+        //~Archivos()
+        public void Dispose()
+        {
+            CerrarArchivos();
+            Console.WriteLine("Finaliza compilacion de Prueba.txt");
+        }
+        private void CerrarArchivos()
+        {
+            archivo.Close();
+            bitacora.Close();
+        }
+        public void Display()
+        {
+            while (!archivo.EndOfStream)
+            {
+                Console.Write((char)archivo.Read());
+            }
+        }
+        public void Load()
+        {
+            while (!archivo.EndOfStream)
+            {
+                bitacora.Write((char)archivo.Read());
+            }
+        }
+        public void Encrypt()
+        {
+            while (!archivo.EndOfStream)
+            {
+                char c;
+                if (char.IsLetter(c = (char)archivo.Read()))
+                {
+                    bitacora.Write((char)(c+1));
+                }
+                else
+                {
+                    bitacora.Write(c);
+                }
+            }
+        }
+        public void Palabra()
+        {
+            char c;
+            string palabra = "";
+
+            while (char.IsWhiteSpace(c = (char)archivo.Read()))
+            {
+            }
+            if (char.IsLetter(c))
+            {
+                palabra += c; //palabra = palabra + c;
+                while (char.IsLetter(c = (char)archivo.Read()))
+                {
+                    palabra += c;
+                }
+                bitacora.WriteLine("palabra = " + palabra);
+            }
+
+        }
+        public bool FinArchivo()
+        {
+            return archivo.EndOfStream;
+        }
+    }
+}
---
+++ Lexico0/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexico0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Archivos a = new Archivos())
+                {
+                    while (!a.FinArchivo())
+                    {
+                        a.Palabra();
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //x = 200;
+            Console.ReadKey();
+        }
+    }
+
+    
+}
